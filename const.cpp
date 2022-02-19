f3d3331819fb3566a6bfb6be60a512c72e09b82f
crizskuk <crizskuk@gmail.com>
2022-02-19 13:34:54 -0500

    --

--- const.cpp
+++ const.cpp
@@ -3,8 +3,9 @@
 
 class hacker {
 public:
+	mutable uint64_t dd = 1;
 	static uint64_t edad;
-	mutable uint64_t code = NULL;		//poude ser modificado por un metodo const
+	mutable uint64_t code = NULL;		//poude ser modificado por un metodo const, porque tiene la plabra clave mutable
 	hacker() : code(1) {
 
 	}
@@ -12,7 +13,7 @@ public:
 		CC("gracias por invocarme");
 	}
 	uint64_t _info(uint64_t& x) {
-		return x + 0xfff;
+		return (x + 0xfff);
 	}
 
 	uint64_t _info(uint64_t& y) const {
@@ -20,12 +21,16 @@ public:
 		return this->code;
 	}
 
-	 uint32_t what(uint32_t x) const{
-		 this->edad = 12;
+	 uint64_t what(void) const{
 		 return this->code;
 	}
 };
 
+uint64_t data(const hacker& h) {
+	h.dd = 5;				//no podemos modificarlo porque el objeto esta en modo lectura
+							//pero si el miembro esta especificado como mutable que significa que podemos modificarlo dentro de un miembro const o funcion de lectura
+	return h.edad+5;
+}
 
 
 int main(void) {
@@ -36,6 +41,6 @@ int main(void) {
 	CC("edad: " << h2._info(edad));
 	h2.code = 30;			//si nuestro objeto fue declarado como const no podemos modificarlo despues de  construccion
 							//pero si le agregamos la palabra clave mutable significa que un metodo const podria modificarlo o una funcion
-	h2.what(5);
+	h2.what();
 	return 0;;
 }
