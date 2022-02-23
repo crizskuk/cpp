c7670fc1bdd736fd8529a4372c54ab85f62d6425
cristian kook <crizskuk@gmail.com>
2022-02-23 03:53:12 -0500

    a

--- explicit.cpp
+++ explicit.cpp
@@ -2,9 +2,34 @@
 
 class hacker {
 public:
+	int edad, y;
+	std::string h_name;
+	explicit hacker(uint8_t age) : edad(age), h_name("desconodido"), y(0) {
+	}
+	explicit hacker(std::string name) : h_name(name), edad(-1), y(0) {
+	}
 
+	void info(void) {
+		std::cout << "edad: " << this->edad << std::endl;
+	}
 };
 
+void info(const hacker& h1) {
+	std::cout << "hacker_edad: " << h1.edad << std::endl;
+	std::cout << "offset: " << &h1 << std::endl;
+}
+//}void info(int f){
+//	std::cout << "f_edad: " << f << std::endl;
+//}
+
 int main(void) {
+	hacker h1(21);
+	info(h1);
+	//info(23);		//llamada implicita se pasa un int pero la funcion recibe un object, entonces porque funciona
+					//pues el compilador realzia una conversion de tipo crea el obejct hacker
+					//
+
+	//hacker h2 = 21;	// 
+	//h2.info();
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
-void info_(const kpop& k);
+//void info_(const kpop& k);
+//
+//class kpop {
+//public:
+//	uint8_t x, y;
+//	kpop(uint8_t x, uint8_t y) : x(x), y(y) {
+//	}
+//
+//	uint8_t info(void) const{
+//		const kpop *e = this;
+//		info_(*this);
+//	}
+//};
+//
+//void info_(const kpop& k) {
+//	std::cout << "x: " << k.x << std::endl;
+//}
+//
+//int main(void) {
+//	kpop kp(12,5);
+//	return 0;
+//}
 
-class kpop {
-public:
-	uint8_t x, y;
-	kpop(uint8_t x, uint8_t y) : x(x), y(y) {
-	}
-
-	uint8_t info(void) const{
-		const kpop *e = this;
-		info_(*this);
-	}
-};
-
-void info_(const kpop& k) {
-	std::cout << "x: " << k.x << std::endl;
-}
-
-int main(void) {
-	kpop kp(12,5);
-	return 0;
-}
