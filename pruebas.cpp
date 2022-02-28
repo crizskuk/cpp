cd2a7789cc6ca26e3f38f0772c2422ac17879129
cristian kook <crizskuk@gmail.com>
2022-02-28 16:39:49 -0500

    struct

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,39 +1,19 @@
-#include <iostream>
-
-
-using namespace std;
-
-class idol {
-public:
-	int edad;
-	idol() : edad(77) {
-		std::cout << "i'am idol:: gracias" << std::endl;
-	}
-	void info_(void) {
-		std::cout << "code: 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a" << std::endl;
-	}
-};
-
-class hacker {
-private:
-	int code = 0xFACC;
-	idol* v1;		//se declara un puntero a la clase v1 pero no se llama al constructor
-public:
-	hacker(idol* i1) : v1(i1){
-		cout << "i'am hacker:: gracias" << endl;
-	}
-	void print(void) const{
-		std::cout << "hello!" << std::endl;
-	}
-
-	idol* obj_(void) const{
-		return v1;
-	}
-};
-
-int main(void) {
-	hacker v1(new idol());
-	cout << v1.obj_()->edad << endl;
-	v1.obj_()->info_();
-	return 0;
-}
+//#include <iostream>
+//
+//typedef struct hacker_inf {
+//	char name[64];
+//	unsigned int edad = 5;
+//	float x;
+//	double y;
+//
+//	hacker_inf(void) {
+//		std::cout << "i'am hacker_inf:: " << std::endl;
+//	}
+//}_hack;
+//
+//int main(void) {
+//	_hack v1;
+//	v1.edad+= 21;
+//	std::cout << "edad: " << v1.edad << std::endl;
+//	return 0;
+//}
---
+++ struct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+typedef struct hacker_inf {
+	char name[64];
+	unsigned int edad = 5;
+	float x;
+	double y;
+
+	hacker_inf(void) {
+		std::cout << "i'am hacker_inf:: " << std::endl;
+	}
+}_hack;
+
+int main(void) {
+	_hack v1;
+	v1.edad += 21;
+	std::cout << "edad: " << v1.edad << std::endl;
+	return 0;
+}
