5f1ad70c01bff868d7244a5bb34b20175b840733
cristian <98377008+crizskuk@users.noreply.github.com>
2022-03-06 19:52:14 -0500

    update default miembros funct

--- defaul_func.cpp
+++ defaul_func.cpp
@@ -14,9 +14,16 @@ void hacker::info(){
     std::cout << "hello i'am constructor";
 }
 
+uint32_t code = NULL;
+
+uint32_t& chang(void){
+    return code;
+}
+
 int main(void){
     hacker v1;
     v1.info();
+    chang() = 0x414141;     //rquivalente ha code = 0x414141;
     // hacker v2 = v1;          //failed porque estamos intentando copia el objeto
                                 //con el operador = que nos dara delete
     return 0;
