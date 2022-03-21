d69df4d8187609e0d4a2c5cb7a78a304e65114bc
cristian <98377008+crizskuk@users.noreply.github.com>
2022-03-21 13:30:37 -0500

    file:Read v2

--- file_read.cpp
+++ file_read.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
+#include <vector>
+
+void mostrar_valor(std::vector<int> &v){
+    for(auto &i : v){
+        std::cout << i << std::endl;
+    }
+}
 
 int main(void){
+    std::vector<int> vbuf;
     std::string buf;
     std::ifstream file("data.txt");
     if(!file){
         std::cout << "failed open file\n";
         exit(EXIT_FAILURE);
     }
-    file >> buf;
-    std::cout << buf << std::endl;
-    for(auto &x : buf){
-        std::cout << x << " -> " << std::hex << "0x" << (int)static_cast<uint8_t>(x) << std::endl;
+    uint16_t elemnts = 18;
+    uint16_t valor;
+    vbuf.reserve(elemnts);
+    for(uint16_t i = 0; i < elemnts; i++){
+        file >> valor;
+        vbuf.push_back(valor);
     }
+    mostrar_valor(vbuf);
     return 0;
 }
